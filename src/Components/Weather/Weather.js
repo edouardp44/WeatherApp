eea4698fa00d13b30e064e434d5578ceff2d3828
Edouard <edouard.p44@gmail.com>
2019-08-29 17:31:06 +0200

    add search

---
+++ src/Components/Weather/Weather.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { EROFS } from 'constants';
+
+class Weather extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            temp: 0,
+            humidity: 0,
+            windSpeed: 0,
+            cityName: '',
+            iconId: '',
+            value: '',
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    componentDidMount() { 
+        navigator.geolocation.getCurrentPosition((success) => {
+            fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${success.coords.latitude}&lon=${success.coords.longitude}&APPID=fcef1558e0f353f59ebb3001bb08d820`)
+            .then(res => res.json())
+            .then((res) => {
+                this.setState({
+                    humidity: res.main.humidity,
+                    cityName: res.name,
+                    windSpeed: res.wind.speed,
+                    temp: res.main.temp,
+                    iconId: res.weather[0].icon
+                })
+            })    
+        }, (error) => {
+            console.log(error);
+        });
+    }
+
+    handleChange(event) {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${event.target.value},fr&APPID=fcef1558e0f353f59ebb3001bb08d820`)
+        .then(result => result.json())
+        .then((result) => {
+            console.log(result)
+        });
+    }
+
+    handleSubmit(event) {
+        alert('Le nom a été soumis : ' + this.state.value);
+        event.preventDefault();
+      }
+
+    render() {
+        return (
+            <div>
+                 <form action='' methode='get' onSubmit={this.handleSubmit}>
+                    <label for="city">
+                    Ville :
+                    </label>
+                    <input type="text" name='city' value={this.state.value} onChange={this.handleChange} />
+                    <input type="submit" value="Envoyer"/>
+                </form>
+
+                <p>Météo</p><img src={`http://openweathermap.org/img/wn/${this.state.iconId}.png`}></img>
+                <p>Vous êtes à {this.state.cityName}</p>
+                <p>Le vent est a une vitesse de {this.state.windSpeed}m/s </p>
+                <p>Il fait actuellement {this.state.temp}°C</p>
+                <p>Le taux d'humidité est de {this.state.humidity}%</p>
+            </div>
+        )
+    }
+}
+export default Weather;
